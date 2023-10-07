5053b724a4493dd0abdbd8898d17285ac173b95a
ritaCanavarro <ritinhamcm@gmail.com>
2023-10-07 16:42:44 +0100

    [FEAT] Prometheus metrics

--- sre/DocumentKeeper/internal/api/documentfetcherapi.go
+++ sre/DocumentKeeper/internal/api/documentfetcherapi.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/version"
 	"github.com/sirupsen/logrus"
@@ -22,17 +23,45 @@ const serverPort = 3000
 
 var router = mux.NewRouter()
 
+// -------------------- Metrics -----------------------
+
+var sucessCountDocumentFetcher = promauto.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "documentKeeper",
+		Name:      "document_fetcher_documents_success_total",
+		Help:      "Total number of documents served successfully.",
+	},
+)
+
+var corruptedCountDocumentFetcher = promauto.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "documentKeeper",
+		Name:      "document_fetcher_documents_corrupted_total",
+		Help:      "Total number of documents that were corrupted.",
+	},
+)
+
+var errorCountDocumentFetcher = promauto.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "documentKeeper",
+		Name:      "document_fetcher_documents_error_total",
+		Help:      "Total number of documents that weren't served due to errors.",
+	},
+)
+
 // -------------------- Auxiliar Functions -----------------------
 
 func processIdentifier(params map[string]string) int {
 	id, err := strconv.Atoi(params["id"])
 
 	if err != nil {
+		errorCountDocumentFetcher.Inc()
 		logrus.Errorf("Identifier is not a valid integer %v", err)
 		return -1
 	}
 
 	if id < 0 {
+		errorCountDocumentFetcher.Inc()
 		logrus.Errorf("Identifier is not a positive integer %v", err)
 		return -1
 	}
@@ -71,6 +100,8 @@ func GetDocument(rw http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get(requestURL)
 
 	if err != nil {
+		errorCountDocumentFetcher.Inc()
+
 		logrus.Errorf("Failed to fetch document from %s", requestURL)
 		configureHttpResponse(rw, http.StatusFailedDependency, "Failed to fetch document from API.")
 		return
@@ -79,6 +110,8 @@ func GetDocument(rw http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(resp.Body)
 
 	if err != nil {
+		errorCountDocumentFetcher.Inc()
+
 		logrus.Errorf("Failed to fetch document from %s", requestURL)
 		configureHttpResponse(rw, http.StatusUnprocessableEntity, "Failed to read document from API.")
 		return
@@ -87,6 +120,8 @@ func GetDocument(rw http.ResponseWriter, r *http.Request) {
 	mimeType := http.DetectContentType(body)
 
 	if mimeType != "application/pdf" && mimeType != "image/png" {
+		errorCountDocumentFetcher.Inc()
+
 		logrus.Errorf("Api served an unexpected document type.")
 		configureHttpResponse(rw, http.StatusUnsupportedMediaType, "Document is not supported.")
 		return
@@ -103,12 +138,15 @@ func GetDocument(rw http.ResponseWriter, r *http.Request) {
 	err = os.WriteFile(filename, body, 0644)
 
 	if err != nil {
+		corruptedCountDocumentFetcher.Inc()
+
 		logrus.Errorf("Api served a corrupted document.")
 		configureHttpResponse(rw, http.StatusUnprocessableEntity, "Document is corrupted.")
 		return
 	}
 
 	http.ServeFile(rw, r, filename)
+	sucessCountDocumentFetcher.Inc()
 }
 
 func NewHttpServer(port string) (*http.Server, error) {
